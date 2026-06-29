82505507e6f06435c9a940f341ab93836fe3b84a
Bruno Gomez <br1_gio@hotmail.com>
2026-06-29 08:27:21 -0600

    chore: add page title and football favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,6 +26,13 @@ export default defineNuxtConfig({
       mode: 'out-in',
     },
     head: {
+      title: 'Quiniela Mundial 2026',
+      link: [
+        {
+          rel: 'icon',
+          href: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">⚽</text></svg>',
+        },
+      ],
       htmlAttrs: {
         'data-theme': 'dark',
       },
